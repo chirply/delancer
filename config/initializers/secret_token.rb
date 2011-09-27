2b8ac719024c765eca1a7ea749fce261c1b49228
Neel Palrecha <neel.palrecha@gmail.com>
2011-09-27 10:53:29 -0700

    Initial commit of rails 3.1 code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Delancer::Application.config.secret_token = '8af2caacbb9f5f8564214c54f17584897579b7ae833521822bfecdebdd905bfe577c0e7b2aa64ab864324cafe87723bbbbdf62fa9a75d59db96cd734ecf9dc0b'
